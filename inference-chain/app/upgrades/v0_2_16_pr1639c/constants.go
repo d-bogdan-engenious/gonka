751f824009bb48b4fc56edd08f2d8b84baca9ddd
dabogdan <dbogdan@engenious.io>
2026-08-27 08:03:12 +0200

    chore(upgrade): v0.2.16-pr-1639c handler for updated pr-1639 (spendable cap + stale-timeout fix)

---
+++ inference-chain/app/upgrades/v0_2_16_pr1639c/constants.go
@@ -0,0 +1,3 @@
+package v0_2_16_pr1639c
+
+const UpgradeName = "v0.2.16-pr-1639c"
---
+++ inference-chain/app/upgrades/v0_2_16_pr1639c/upgrades.go
@@ -0,0 +1,23 @@
+package v0_2_16_pr1639c
+
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/productscience/inference/x/inference/keeper"
+	"github.com/productscience/inference/x/inference/types"
+)
+
+func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator, k keeper.Keeper) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		k.LogInfo("starting upgrade", types.Upgrades, "version", UpgradeName)
+		toVM, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return toVM, err
+		}
+		k.LogInfo("successfully upgraded", types.Upgrades, "version", UpgradeName)
+		return toVM, nil
+	}
+}
